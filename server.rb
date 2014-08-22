276d794094667c9f79f3c012f9fd9f3a8a0cd60f
rachelbookhout <rbookhou@gmail.com>
2014-08-22 08:36:32 -0400

    add links

---
+++ server.rb
@@ -0,0 +1,56 @@
+require 'sinatra'
+# #require 'sinatra-reloader'
+require 'pry'
+require 'csv'
+enable :sessions
+
+def read_csv(filename)
+  data = []
+  CSV.foreach(filename, headers:true) do |row|
+    data << row.to_hash
+  end
+  data
+end
+
+
+def validate_url(filename,url)
+  checked_urls = filename.select do |row|
+    row["Url"] == url
+  end
+  checked_urls
+end
+
+before do
+  @articles = read_csv('articles.csv')
+end
+
+
+get '/' do
+  @articles = read_csv('articles.csv')
+  erb :index
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/new' do
+
+  # Read the input from the form the user filled out
+  @name = params["article_name"]
+  @url = params['url']
+  @description = params['description']
+  @checked_urls = validate_url(@articles, @url)
+  if @checked_urls.empty?
+  # Open the "tasks" file and append the task
+    CSV.open('articles.csv', 'a') do |csv|
+      csv << [@name, @url, @description]
+    end
+    # Send the user back to the home page which shows
+    # the list of articles
+    redirect '/'
+  else
+    session[:message] = "We already know about this. Send us something different"
+    redirect '/new'
+  end
+end
