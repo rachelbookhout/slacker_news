401a0bda62bdcf2fd9928d5643dbd1401b389471
rachelbookhout <rbookhou@gmail.com>
2014-08-22 11:17:27 -0400

    Add GemFile, ProcFile, and made it Heroku ready

--- server.rb
+++ server.rb
@@ -2,7 +2,49 @@ require 'sinatra'
 # #require 'sinatra-reloader'
 require 'pry'
 require 'csv'
+require 'redis'
 enable :sessions
+
+def get_connection
+  if ENV.has_key?("REDISCLOUD_URL")
+    Redis.new(url: ENV["REDISCLOUD_URL"])
+  else
+    Redis.new
+  end
+end
+
+require 'sinatra'
+require 'redis'
+require 'json'
+
+def get_connection
+  if ENV.has_key?("REDISCLOUD_URL")
+    Redis.new(url: ENV["REDISCLOUD_URL"])
+  else
+    Redis.new
+  end
+end
+
+def find_articles
+  redis = get_connection
+  serialized_articles = redis.lrange("slacker:articles", 0, -1)
+
+  articles = []
+
+  serialized_articles.each do |article|
+    articles << JSON.parse(article, symbolize_names: true)
+  end
+
+  articles
+end
+
+def save_article(url, title, description)
+  article = { url: url, title: title, description: description }
+
+  redis = get_connection
+  redis.rpush("slacker:articles", article.to_json)
+end
+
 
 def read_csv(filename)
   data = []
